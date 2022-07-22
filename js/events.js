1d70fe70a607d162c85a11daf79881633995a212
codewithdripzy <codewithdripzy@gmail.com>
2022-07-23 00:27:03 +0100

    Completed the login and register scripts

--- js/events.js
+++ js/events.js
@@ -5,24 +5,34 @@ function Login() {
   LoginRequest(email, password);
 }
 
+function checkPasswordLength(){
+  let psw = document.getElementById('rgstpsw');
+
+  if(psw.value.length < 8){
+    showAlert("alert-caution", "Password must not be less than 8 characthers");
+  }
+}
+
+function validatePassword(){
+  let psw = document.getElementById('rgstpsw');
+  let confirmpassword = document.getElementById('confirmpsw');
+
+  if (psw.value != confirmpassword.value) {
+    showAlert('alert-caution', "The passwords you provided do not match!");
+  }
+}
 function SignUp() {
-  let fullname = document.getElementById('fullname')
+  let fullname = document.getElementById('fullname');
   let email = document.getElementById('email');
-  let psw = document.getElementById('rgstpsw')
-  let confirmpassword = document.getElementById('confirmpsw')
-  let alert = document.getElementById('alert')
+  let psw = document.getElementById('rgstpsw');
+  let confirmpassword = document.getElementById('confirmpsw');
+  let alert = document.getElementById('alert');
 
-  SignupRequest(fullname.value, email.value, psw.value)
-
-  console.log(psw.length);
-  if (psw.length < 8) {
+  if (psw.value.length >= 8) {
     if (psw.value != confirmpassword.value) {
-      SignupRequest(fullname, email, psw)
+      showAlert('alert-danger', "The passwords you provided do not match!");
     } else {
-      alert.innerHTML =
-        "<i class='fa fa-info-circle'></i> Password does not match!"
-      alert.style.display = 'block'
-      alert.classList.add('alert-danger')
+      SignupRequest(fullname.value, email.value, psw.value)
     }
   } else {
     alert.innerHTML = "<i class='fa fa-info-circle'></i> Password must not be less than 8 characters"
