7639cfcc2d0cff3ae9f7d9b26c0d5bb803c2e39f
codewithdripzy <codewithdripzy@gmail.com>
2022-08-10 01:32:12 +0100

    Made Changes to Docs

--- api/createAccount.php
+++ api/createAccount.php
@@ -1,54 +1,79 @@
 <?php
-if ($_REQUEST) {
-    include_once '../config/database.php';
-    include_once '../models/user.php';
+    if ($_REQUEST) {
+        include_once '../config/database.php';
+        include_once '../models/user.php';
+        include_once '../models/referrals.php';
 
-    $database = new Database();
-    $db = $database->getConnection();
 
-    $user = new User($db);
+        $database = new Database();
+        $db = $database->getConnection();
 
-    $user->fullname = $_REQUEST['fullname'];
-    $user->email = $_REQUEST['email'];
-    $user->password = $_REQUEST['password'];
-    $user->access_level = 'user';
-    $user->access_code = '';
-    $user->status = 0;
+        $user = new User($db);
+        $referral = new Referral($db);
 
-    if (isset($_REQUEST['fullname']) && !empty($_REQUEST['fullname']) && isset($_REQUEST['email']) && !empty($_REQUEST['email']) && isset($_REQUEST['password']) && !empty($_REQUEST['password'])) {
-        if($user->emailExist($_REQUEST['email'])){
-            $res = array();
+        $user->fullname = $_REQUEST['fullname'];
+        $user->email = $_REQUEST['email'];
+        $user->password = $_REQUEST['password'];
+        $user->access_level = 'user';
+        $user->access_code = '';
+        $user->status = 0;
 
-            $res['message'] = "This Email has been registered!";
-            $res['err'] = 301;
-            $res['state'] = false;
+        if (isset($_REQUEST['fullname']) && !empty($_REQUEST['fullname']) && isset($_REQUEST['email']) && !empty($_REQUEST['email']) && isset($_REQUEST['password']) && !empty($_REQUEST['password'])) {
+            if($user->emailExist($_REQUEST['email'])){
+                $res = array();
 
-            $response = json_encode($res);
-
-            echo $response;
-            return $response;
-        }else{
-            $res = array();
-
-            if ($user->createAccount()) {
-                $_REQUEST = array();
-                
-                $res['message'] = "Account has been sucessfully created!";
-                $res['err'] = null;
-                $res['state'] = true;
-
-            } else {
-                $res['message'] = "Something went wrong! That's all we know.";
-                $res['err'] = 404;
+                $res['message'] = "This Email has been registered!";
+                $res['err'] = 301;
                 $res['state'] = false;
+
+                $response = json_encode($res);
+
+                echo $response;
+                return $response;
+            }else{
+                $res = array();
+
+                if ($user->createAccount()) {
+                    if(isset($_REQUEST['referral']) && !empty($_REQUEST['referral'])){
+                        if($user->emailExist($_REQUEST['referral'])){
+                        
+                            $referral->referral_id = $user->id;
+
+                            $user->emailExist($_REQUEST['email']);
+    
+                            $referral->referred_id = $user->id;
+    
+                            $referral->token = sha1(rand(100, 1000));
+
+                            if($referral->referUser()){
+                                $res['referrals_msg'] = "Referral Created!";
+                            }else{
+                                $res['referrals_msg'] = "Something went wrong thats all we know!";
+                            }
+                        }else{
+                            $res['referrals_msg'] = "Unable to refer user, invalid referral link!";
+                        }
+                    }
+                
+                    $_REQUEST = array();
+                    
+                    $res['message'] = "Account has been sucessfully created!";
+                    $res['err'] = null;
+                    $res['state'] = true;
+
+                } else {
+                    $res['message'] = "Something went wrong! That's all we know.";
+                    $res['err'] = 404;
+                    $res['state'] = false;
+                }
+
+                $response = json_encode($res);
+
+                echo $response;
+                return $response;
             }
-
-            $response = json_encode($res);
-
-            echo $response;
-            return $response;
+        } else {
+            echo "Invalid Request!";
         }
-    } else {
-        echo "Invalid Request!";
     }
-}
+?>
